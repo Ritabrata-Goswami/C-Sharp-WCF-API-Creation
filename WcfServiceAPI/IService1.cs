e1061cdbc07335d67745d060ac53fcbaf1da5366
Ritabrata-Goswami <1995ritabrata@gmail.com>
2024-08-19 00:11:09 +0530

    Created basic WCF API service.

---
+++ WcfServiceAPI/IService1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceAPI
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        // TODO: Add your service operations here
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/GetData/{value}", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        string GetData(string value);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/PayBill/{PayId}", BodyStyle = WebMessageBodyStyle.Wrapped, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string PayBill(string PayId);
+    }
+
+}
